005b754031839642565e525d4eff09b1174f1ff5
indinin <55898529+indinin@users.noreply.github.com>
2021-06-11 15:36:05 -0700

    have something that sort of works

---
+++ Assets/Collin/Scripts/makeMesh.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeMesh : MonoBehaviour
+{
+    public GameObject[] nodes;
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            updateNodes();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            generateMesh();
+        }
+    }
+
+    void updateNodes()
+    {
+        nodes = GameObject.FindGameObjectsWithTag("webNode");
+    }
+
+    void generateMesh()
+    {
+        GetComponent<MeshFilter>().mesh = mesh;
+        createShape();
+    }
+
+    void createShape()
+    {
+        vertices = new Vector3[nodes.Length];
+        int i = 0;
+        foreach (GameObject node in nodes)
+        {
+            vertices[i] = node.transform.position;
+            i++;
+        }
+        triangles = new int[]
+        {
+            0,1,2
+        };
+
+        updateMesh();
+    }
+
+    void updateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
---
+++ Assets/Collin/Scripts/spawnNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnNode : MonoBehaviour
+{
+    
+    public Camera cam;
+    public GameObject obj, tracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                tracker.transform.position = hit.point;
+                Instantiate(obj, tracker.transform.position, tracker.transform.rotation);
+            }
+        }
+    }
+}
