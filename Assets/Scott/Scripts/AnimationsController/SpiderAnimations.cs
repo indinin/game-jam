9972d05496e56cb943872b31882e3827867de349
ScottOgar <56742577+ScottOgar@users.noreply.github.com>
2021-06-11 18:01:43 -0700

    Made Animation Script for Spider
    Added 4 functions to control 3 animations for spider.

---
+++ Assets/Scott/Scripts/AnimationsController/SpiderAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnimations : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    public void MovingAnim()
+    {
+        animator.SetBool("moving", true);
+        Debug.Log("Moving");
+    }
+
+    public void StopMovingAnim()
+    {
+        animator.SetBool("moving", false);
+        Debug.Log("Not Moving");
+    }
+
+    public void JumpAnim()
+    {
+        animator.SetTrigger("jump");
+        Debug.Log("Jumping");
+    }
+
+    public void DeathAnim()
+    {
+        animator.SetTrigger("dead");
+        Debug.Log("SPLAT!");
+    }
+}
