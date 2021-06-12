d81396fd14ca23595d50a0d1585ac1a9faa547aa
indinin <55898529+indinin@users.noreply.github.com>
2021-06-11 20:58:25 -0700

    web meter script added

---
+++ Assets/Collin/Scripts/webMeterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webMeterScript : MonoBehaviour
+{
+    public float web, lenience;
+    bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(web + lenience < 0 && !gameOver)
+        {
+            print("Game Over");
+            gameOver = true;
+        }
+    }
+
+    public void changeWebbing(float amount)
+    {
+        web += amount;
+    }
+}
