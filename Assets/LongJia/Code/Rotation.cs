532443f43a93b1d958d137cfc23ceb8e0b71859a
LongJia Lin <jia1191878105@gmail.com>
2021-06-11 23:59:24 -0700

    rotate and movement

---
+++ Assets/LongJia/Code/Rotation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (transform.position.x >= 12f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        if (transform.position.x <= -12f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+
+        if (transform.position.y >= 5.5f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        if (transform.position.y <= -5.5f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+      
+    }
+}
---
+++ Assets/LongJia/Code/playerScreenBound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScreenBound : MonoBehaviour
+{
+    public Vector2 screenBounds;
+    public float objWidth;
+    public float objHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 objectPosition = transform.position;
+        objectPosition.x = Mathf.Clamp(objectPosition.x, -screenBounds.x + objWidth, screenBounds.x - objWidth);
+        objectPosition.y = Mathf.Clamp(objectPosition.y, -screenBounds.y + objHeight, screenBounds.y - objHeight);
+        transform.position = objectPosition;
+    }
+}
