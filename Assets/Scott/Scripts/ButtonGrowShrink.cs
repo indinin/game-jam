5d4911dfe5c22a8d1cb33d98935a19f825827b2b
ScottOgar <56742577+ScottOgar@users.noreply.github.com>
2021-06-11 18:49:38 -0700

    Added Button Grow and Shrink
    Added growing and shrinking to the buttons.

---
+++ Assets/Scott/Scripts/ButtonGrowShrink.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonGrowShrink : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private bool isShrunkAndInactive = false;
+
+    void Start()
+    {
+        if(this.GetComponent<RectTransform>() != null)
+        {
+            rectTransform = this.GetComponent<RectTransform>();
+        }
+    }
+
+    void Update()
+    {
+        if(!this.gameObject.activeSelf && !isShrunkAndInactive)
+        {
+            Shrink();
+            isShrunkAndInactive = true;
+        }
+
+        if(this.gameObject.activeSelf && isShrunkAndInactive)
+        {
+            isShrunkAndInactive = false;
+        }
+    }
+
+    public void Grow()
+    {
+        if(rectTransform)
+        {
+            rectTransform.localScale = new Vector3(1.4f, 1.4f, 1);
+        }
+    }
+
+    public void Shrink()
+    {
+        if(rectTransform)
+        {
+            rectTransform.localScale = new Vector3(1f, 1f, 1);
+        }
+    }
+}
